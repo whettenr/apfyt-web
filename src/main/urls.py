5211d8d1a0765db111fea0a5f622da926435e6b0
Josh Payne <joshpayne1@rescomp-16-335460.stanford.edu>
2017-02-19 23:42:24 -0800

    some minor style changes

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 from .views import contact 
 urlpatterns = [
-	url(r'^$', TemplateView.as_view(template_name="home.html")),
-	url(r'^about/$', TemplateView.as_view(template_name="about.html")),
+	url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
+	url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
     url(r'^contact/$', contact, name='contact'),
     ]
