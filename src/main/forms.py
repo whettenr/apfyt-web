8686e24d335165d53342b8ccadcbaa6d84c4c912
Josh Payne <joshpayne1@DN0a23337f.SUNet>
2017-02-23 16:54:31 -0800

    css

--- src/main/forms.py
+++ src/main/forms.py
@@ -1,16 +1,18 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    contact_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Name'}))
-    contact_email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-    contact_company = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Company'}))
-    contact_subject = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Subject'}))
+    contact_firstname = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'First', 'class': 'form-control-two'}))
+    contact_lastname = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Last', 'class': 'form-control-two'}))
+    contact_email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control-two'}))
+    contact_company = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Company', 'class': 'form-control-two'}))
+    contact_subject = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Subject', 'class': 'form-control'}))
     content = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': 'Message'}))
     
 
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
-        self.fields['contact_name'].label = "Name "
+        self.fields['contact_firstname'].label = "Name "
+        self.fields['contact_lastname'].label = ""
         self.fields['contact_email'].label = "Email"
         self.fields['contact_company'].label = "Company"
         self.fields['contact_subject'].label = "Subject"
